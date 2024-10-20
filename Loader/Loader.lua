425d13a8c93ce8dd7d7ed3e92b158463eeca520f
x2Sunz <116784765+x2Sunz@users.noreply.github.com>
2024-10-20 10:49:04 +0700

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,7 +1,7 @@
-if game.PlaceId == 2753915549 or 4442272183 or 7449423635  and getgenv().Mode == "Bounty" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Sunz/Love-Trash/refs/heads/main/Blox%20Fruit/Bounty/Loader.lua"))()
-end
-if game.PlaceId == 2753915549 or 4442272183 or 7449423635  and not getgenv().Mode == "Bounty" then
+if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+    if getgenv().Mode == "Bounty" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Sunz/Love-Trash/refs/heads/main/Blox%20Fruit/Bounty/Loader.lua"))()
+    else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Sunz/Love-Trash/refs/heads/main/Blox%20Fruit/Loader.lua"))()
 end
 if game.PlaceId == 13772394625 then -- Blade Ball
